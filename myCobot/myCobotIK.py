6f26d5226b385439668f333f515d74ef384460d6
Takuya Ikeda <takuyaikeda2533@gmail.com>
2020-12-24 23:13:34 +0900

    modify roll pitch yaw permutation

--- myCobot/myCobotIK.py
+++ myCobot/myCobotIK.py
@@ -12,14 +12,14 @@ a7 = [0, 0, 0]
 vector_list = [a1, a2, a3, a4, a5, a6, a7]
 
 # Link Length
-b1 = [0., 0., 0.06114]
-b2 = [0., 0., 0.07042]
-b3 = [0., 0., 0.1104]
-b4 = [0., 0., 0.0960]
-b5 = [0., 0.06639, 0.]
-b6 = [0., 0., 0.07318]
-b7 = [0.0436, 0., 0.]
-length_list = sp.Matrix([b1, b2, b3, b4, b5, b6, b7])
+# b1 = [0., 0., 0.06114]
+# b2 = [0., 0., 0.07042]
+# b3 = [0., 0., 0.1104]
+# b4 = [0., 0., 0.0960]
+# b5 = [0., 0.06639, 0.]
+# b6 = [0., 0., 0.07318]
+# b7 = [0.0436, 0., 0.]
+# length_list = sp.Matrix([b1, b2, b3, b4, b5, b6, b7])
 
 # Sympy/Numpy
 # ref: https://qiita.com/tibigame/items/539f4f9ed0b6d8d76946
@@ -58,6 +58,16 @@ def rodrigues(vector, angle):
 if __name__ == "__main__":
     sp.init_printing()
     sp.var('x, y, z, roll, pitch, yaw, ang1, ang2, ang3, ang4, ang5, ang6')
+    sp.var('b1, b2, b3, b4, b5, b6, b7')
+    c1 = [0., 0., b1]
+    c2 = [0., 0., b2]
+    c3 = [0., 0., b3]
+    c4 = [0., 0., b4]
+    c5 = [0., b5, 0.]
+    c6 = [0., 0., b6]
+    c7 = [b7, 0., 0.]
+    length_list = sp.Matrix([c1, c2, c3, c4, c5, c6, c7])
+
     # sp.var('x, y, a, b, c, g, h')
     # eq5=sp.Eq(y, a*x**2+b*x+c)
     # eq6=sp.Eq(y, g*x+h)
@@ -66,10 +76,20 @@ if __name__ == "__main__":
     # import pdb;pdb.set_trace()
     # print(sol)
 
+    a1 = [0, 0, 1]
+    a2 = [0, 1, 0]
+    a3 = [0, 1, 0]
+    # RyRzRx
+    a4 = [0, 1, 0]
+    a5 = [0, 0, 1]
+    a6 = [1, 0, 0]
+    a7 = [0, 0, 0]
+    vector_list = [a1, a2, a3, a4, a5, a6, a7]
+
     pos = sp.Matrix([[0, 0, 0]]).T
     R = sp.eye(3)
     angle_list = [ang1, ang2, ang3, ang4, ang5, ang6, 0]
-    for i in range(7):
+    for i in range(3, 7):
         pos = pos + R * length_list[i, :].T
         R = R * rodrigues(vector_list[i], angle_list[i])
 
@@ -77,15 +97,31 @@ if __name__ == "__main__":
     R = sp.simplify(R)
 
     # Rrpy = rot_z(yaw) * rot_y(pitch) * rot_x(roll)
-    # Rrpy = rot_z(yaw) * rot_x(pitch) * rot_y(roll)
+    # Rrpy = rot_y(pitch) * rot_z(yaw) * rot_x(roll)
+    # Rrpy = sp.simplify(Rrpy)
 
-    phi = sp.atan2(R[2, 1], R[2, 2])
-    theta = sp.asin(-R[2, 0])
-    psi = sp.atan2(R[1, 0], R[0, 0])
+    psi = sp.asin(R[1, 0])
+    phi = sp.atan2(-R[1, 2], R[1, 1])
+    theta = sp.atan2(R[2, 0], -R[0, 0])
+
+    # import pdb
+    # pdb.set_trace()
+    # phi = sp.atan2(R[2, 1], R[2, 2])
+    # theta = sp.asin(-R[2, 0])
+    # psi = sp.atan2(R[1, 0], R[0, 0])
 
     phi = sp.simplify(phi)
     theta = sp.simplify(theta)
     psi = sp.simplify(psi)
+
+    eq1 = sp.Eq(roll, phi)
+    eq2 = sp.Eq(pitch, theta)
+    eq3 = sp.Eq(yaw, psi)
+
+    # import pdb
+    # pdb.set_trace()
+
+    # sol = sp.solve([eq1, eq2, eq3], [ang4, ang5, ang6])
 
     import pdb
     pdb.set_trace()
