935cfbcb2ebb7078635a5cbf76d1bdeb05629c0d
takuya <robonchu>
2022-05-07 22:10:45 +0900

    add move base ik

--- mobile_manipulation/mm_numerical_ik.py
+++ mobile_manipulation/mm_numerical_ik.py
@@ -12,11 +12,13 @@ L4 = [0., 0., 0.025]
 LINK_LENGTHS = np.array([L1, L2, L3, L4])
 
 # Joint Vector
+J0 = [0, 0, 1]
 J1 = [0, 1, 0]
 J2 = [0, 1, 0]
 J3 = [0, 1, 0]
 J4 = [0, 1, 0]
-JOINT_VECTORS = np.array([J1, J2, J3, J4])
+# JOINT_VECTORS = np.array([J1, J2, J3, J4])
+JOINT_VECTORS = np.array([J0, J1, J2, J3, J4])
 
 
 def skew_mat(vector):
@@ -83,22 +85,34 @@ def calc_mm_ik(base_pos, angle_list,
                target_pos,
                target_rot,
                threshold,
-               max_itr=1000):
+               max_itr=50000):
     alpha = 1.0
-    print("here")
+    # alpha = np.array([0.001, 1.0, 1.0, 1.0, 1.0])
     for i in range(max_itr):
         current_pos, current_rot, pos_list, rot_list = calc_mm_fk(
-            base_pos, angle_list, vector_list, length_list)
-        J = calc_jacobi(vector_list, pos_list[1:], rot_list[1:])
+            base_pos, angle_list, vector_list[1:], length_list)
+        J = calc_jacobi(vector_list, pos_list, rot_list)
         pos_err, rot_err = calc_err(target_pos, target_rot, current_pos,
                                     current_rot)
         err = np.concatenate([pos_err, rot_err[:, 0]])
+        print("iter:", i)
         print("err:", np.linalg.norm(err))
         if np.linalg.norm(err) < threshold:
             break
-        delta_angle = alpha * (np.linalg.pinv(J) @ err)
+        elif np.linalg.norm(err) > 100.:
+            return None, None
+
+        # lam = np.array([1., 0.1, 0.1, 0.1, 0.1])
+        lam = 0.1
+        Jninv = np.linalg.inv((J.T @ J + lam * np.eye(len(J[0])))) @ J.T
+        # delta_angle = alpha * (np.linalg.pinv(J) @ err)
+        delta_base_angle = alpha * (Jninv @ err)
+        delta_base = np.array([delta_base_angle[1], 0, 0])
+        delta_angle = delta_base_angle[1:]
+        base_pos += delta_base
+        angle_list = np.array(angle_list)
         angle_list = np.array(angle_list) + delta_angle
-    return angle_list
+    return base_pos, angle_list
 
 
 def draw_link_position(pos_list, dof=6):
@@ -128,30 +142,37 @@ def draw_link_position(pos_list, dof=6):
 
 
 def run():
-    base_pos = [0., 0., 0.]
+    init_posx = 0.05
+    init_base_pos = [init_posx, 0., 0.]
     rad1 = np.pi/8
     rad2 = np.pi/8
     rad3 = np.pi/8
     rad4 = np.pi/8
     init_angles = np.array([rad1, rad2, rad3, rad4])
     init_pos, init_R, init_pos_list, init_R_list = calc_mm_fk(
-        base_pos, init_angles, JOINT_VECTORS, LINK_LENGTHS)
+        init_base_pos, init_angles, JOINT_VECTORS[1:], LINK_LENGTHS)
 
-    base_pos = [0., 0., 0.]
+    target_posx = 0.06
+    target_base_pos = [target_posx, 0., 0.]
     rad1 = np.pi/8
     rad2 = np.pi/8
     rad3 = np.pi/8
-    rad4 = np.pi/4
+    rad4 = np.pi/7
     target_angles = np.array([rad1, rad2, rad3, rad4])
     target_pos, target_R, target_pos_list, target_R_list = calc_mm_fk(
-        base_pos, target_angles, JOINT_VECTORS, LINK_LENGTHS)
+        target_base_pos, target_angles, JOINT_VECTORS[1:], LINK_LENGTHS)
 
-    angle_list = calc_mm_ik(base_pos, init_angles, JOINT_VECTORS, LINK_LENGTHS, target_pos,
-                            target_R, 0.0001)
+    base_pos, angle_list = calc_mm_ik(init_base_pos, init_angles,
+                                      JOINT_VECTORS, LINK_LENGTHS, target_pos,
+                                      target_R, 0.000005)
 
     pos, R, pos_list, R_list = calc_mm_fk(
-        base_pos, angle_list, JOINT_VECTORS, LINK_LENGTHS)
+        base_pos, angle_list, JOINT_VECTORS[1:], LINK_LENGTHS)
 
+    print("base_pos:", base_pos, "angle_list:", angle_list)
+    print("init_pos:", init_pos, "init_R:", init_R)
+    print("target_pos:", target_pos, "target_R:", target_R)
+    print("final_pos:", pos, "final_R:", R)
     draw_link_position(np.array(pos_list))
 
 
